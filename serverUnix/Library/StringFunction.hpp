41eb5ef07b146252e09c23c1048d664a02e66019
marianWSL <marian.lippold@gmx.de>
2023-08-07 00:49:15 +0200

    added utilities

---
+++ serverUnix/Library/StringFunction.hpp
@@ -0,0 +1,11 @@
+#ifndef StringFunction_hpp
+#define StringFunction_hpp
+
+#include <iostream>
+#include <string>
+
+#endif
+
+std::string tokUntil(std::string str, std::string delimiter);
+
+std::string to_uppercase(std::string str);
