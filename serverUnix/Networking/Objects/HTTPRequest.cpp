50a545973d2601348097703e277751bfc4bd56e5
marianWSL <marian.lippold@gmx.de>
2023-08-07 00:44:42 +0200

    without debug statements

--- serverUnix/Networking/Objects/HTTPRequest.cpp
+++ serverUnix/Networking/Objects/HTTPRequest.cpp
@@ -85,40 +85,18 @@ struct HTTPRequest http_request_constructor(char *request_string)
     request_line = request_line.erase(0, pos + 1);
     request.Method = process_http_method(method);
 
-    // cout << "request.Method: " << request.Method << endl;
-    // cout << "requestline: " << request_line << endl;
-
     // URI
     pos = request_line.find(" ");
     string URI = request_line.substr(0, pos);
     request_line.erase(0, pos + 1);
     request.URI = process_http_uri(URI);
     
-    // cout << "request.URI: " << request.URI << endl;
-    // cout << "requestline: " << request_line << endl;
-
     // HTTPVersion
     string HTTPVersion = request_line;
     request.HTTPVersion = process_http_version(HTTPVersion);
-
-    // cout << request.HTTPVersion << endl;
 
     // headers
     request.HTTPHeaders = process_http_headers(headers);
 
     return request;
-}
-
-int main() {
-
-    char *request_string = "GET /80 HTTP/1.1\nHost: 172.27.34.56\nConnection: keep-alive\nDNT: 1\nUpgrade-Insecure-Requests: 1\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/*;q=0.8,application/signed-exchange;v=b3;q=0.7\ncp-extension-installed: Yes\nAccept-Encoding: gzip, deflate\nAccept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7\nRange: bytes=85-85\nIf-Range: Wed, 19 Jul 2023 19:15:56 GMT\n\n123";
-
-    struct HTTPRequest request = http_request_constructor(request_string);
-
-    // for (const auto& pair : request.HTTPHeaders) 
-    // {
-    //     std::cout << pair.first << ": " << pair.second << endl;
-    // }
-
-    return 0;
 }
