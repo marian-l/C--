fe918d7b1d45a78dcbf7cbcee7fbef2d0cb0f6bd
marianlippold <marian.lippold@students.hs-mainz.de>
2023-09-30 22:11:18 +0200

    needs testing

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by maria on 18.09.2023.
+//
+#include <iostream>
+#include "LeetCode.h"
+
+int main() {
+    LeetCode leetCode = LeetCode();
+    std::string digits = "23";
+    leetCode.CartesianProduct(digits);
+
+        return 0;
+}
