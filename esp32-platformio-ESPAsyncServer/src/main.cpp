03ba4e776e2962721b22c2d301a642d53a83b58d
Marian <marian.lippold@student.hs-rm.de>
2025-01-22 11:54:00 +0100

    added sound and light sensor support and data sending

--- esp32-platformio-ESPAsyncServer/src/main.cpp
+++ esp32-platformio-ESPAsyncServer/src/main.cpp
@@ -10,6 +10,8 @@
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
 
+#define BH1750_ADDRESS 0x23
+
 // Server Socket
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
@@ -40,10 +42,120 @@ float temperature;
 float humidity;
 float pressure;
 float lux;
-float volume;
+int sound; // sound
+
+const int analogPin = A0; // Analog output from the KY-038
+const int digitalPin = 35; // Digital output from the KY-038
 
 void handleNotFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "There is nothing here for you to see");
+}
+
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+        Serial.printf("WebSocket client #%u connected\n", client->id());
+    } else if (type == WS_EVT_DISCONNECT) {
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    } else if (type == WS_EVT_DATA) {
+        // commands are not needed as the http request will always just ask for the page.
+        // onWebSocketMessage(arg, data, len);
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // Wifi Access Point
+    WiFi.softAP(ssid, password);
+
+    // File System
+    if (!LittleFS.begin()) {
+        Serial.println("Fehler beim Mounten von LittleFS");
+        return;
+    }
+
+    if(!Wire.begin()) {
+        Serial.println("Fehler beim Starten des Wire");
+    }
+
+    if(!lightSensor.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x5C)) {
+        Serial.printf("Fehler beim Starten des Lichtsensors");
+    }
+
+    // BME-Sensor
+    // if(!bme280.init()) {
+    //     Serial.println("BME280 konnte nicht gefunden werden!");
+    // }
+
+    pinMode(digitalPin, INPUT);
+
+    // Server
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("test-website.html");
+    // server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+    server.onNotFound(handleNotFound);
+    server.addHandler(&ws);
+    server.begin();
+
+    // Websocket Event Handler
+    ws.onEvent(onWebSocketEvent);
+}
+
+void notifyClients() {
+    String jsonData = "{\"temperature\": " + String(temperature) +
+                      ", \"humidity\": " + String(humidity) +
+                      ", \"light\": " + String(lux) +
+                      ", \"pressure\": " + String(pressure) +
+                      ", \"volume\": " + String(sound) + "}";
+
+    ws.textAll(jsonData); // Send data to all connected WebSocket clients
+}
+
+void loop() {
+    Serial.printf("WS-Connections: %d", ws.count());
+
+    if (ws.count() > 0) {
+        // wie viel Speicher ist noch da?
+        Serial.printf("Free heap: %u bytes\n", ESP.getFreeHeap());
+
+        if (lightSensor.measurementReady()) {
+            lux = lightSensor.readLightLevel();
+            Serial.printf("lux: %f ", lux);
+        }
+
+        if (!bme280.begin(0x76)) {
+            Serial.println("BME280 is not running.");
+        } else {
+            temperature = bme280.readTemperature();
+            humidity = bme280.readHumidity();
+            pressure = bme280.readPressure()/100.0F;
+
+            Serial.printf("temperature: %f ", temperature);
+            Serial.printf("humidity: %f ", humidity);
+            Serial.printf("pressure: %f ", pressure);
+        }
+
+        sound = analogRead(analogPin); // Read the analog value
+        // int digitalValue = digitalRead(digitalPin); // Read the digital value
+        Serial.print("Analog Value: ");
+        Serial.print(sound);
+        // Serial.print(" | Digital Value: ");
+        // Serial.println(digitalValue);
+
+        notifyClients();
+
+        delay(1000);
+    } else {
+        delay(5000);
+    }
+
+    Serial.println("Beep beep booop.");
+}
+
+int main() {
+    setup();
+    while (true) {
+        loop();
+    }
 }
 
 void handleRemoveSensor(uint8_t data) {
@@ -101,86 +213,5 @@ void onWebSocketMessage(void *arg, uint8_t *data, size_t len) {
                 Serial.printf("Unrecognized command: %c \n", main_command);
                 break;
         }
-    }
-}
-
-void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-    if (type == WS_EVT_CONNECT) {
-        Serial.printf("WebSocket client #%u connected\n", client->id());
-    } else if (type == WS_EVT_DISCONNECT) {
-        Serial.printf("WebSocket client #%u disconnected\n", client->id());
-    } else if (type == WS_EVT_DATA) {
-        onWebSocketMessage(arg, data, len);
-    }
-}
-
-void setup() {
-    Serial.begin(115200);
-
-    // Wifi Access Point
-    WiFi.softAP(ssid, password);
-
-    // File System
-    if (!LittleFS.begin()) {
-        Serial.println("Fehler beim Mounten von LittleFS");
-        return;
-    }
-
-    // BME-Sensor
-    if(!bme280.init()) {
-        Serial.println("BME280 konnte nicht gefunden werden!");
-    }
-
-    // Server
-    server.serveStatic("/", LittleFS, "/").setDefaultFile("test-website.html");
-    // server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
-    server.onNotFound(handleNotFound);
-    server.addHandler(&ws);
-    server.begin();
-
-    // Websocket Event Handler
-    ws.onEvent(onWebSocketEvent);
-}
-
-void notifyClients() {
-    String jsonData = "{\"temperature\": " + String(temperature) +
-                      ", \"humidity\": " + String(humidity) +
-                      ", \"light\": " + String(lux) +
-                      " \" pressure\": " + String(pressure) +"}";
-
-    ws.textAll(jsonData); // Send data to all connected WebSocket clients
-}
-
-void loop() {
-    Serial.printf("WS-Connections: %d", ws.count());
-
-    if (ws.count() > 0) {
-        // wie viel Speicher ist noch da?
-        Serial.printf("Free heap: %u bytes\n", ESP.getFreeHeap());
-
-        if (lightSensor.measurementReady()) {
-            lux = lightSensor.readLightLevel();
-        }
-
-        if (!bme280.begin(0x76)) {
-            Serial.println("BME280 is not running.");
-        } else {
-            temperature = bme280.readTemperature();
-            humidity = bme280.readHumidity();
-            pressure = bme280.readPressure()/100.0F;
-        }
-
-        notifyClients();
-    } else {
-        delay(5000);
-    }
-
-    Serial.println("Beep beep booop.");
-}
-
-int main() {
-    setup();
-    while (true) {
-        loop();
     }
 }
--- esp32-platformio/source_files_excluded_from_building_yet/MqttDataTransmission.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by maria on 02.11.2024.
-//
-
